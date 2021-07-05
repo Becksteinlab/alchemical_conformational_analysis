21e810659be5314c4f2098180a4a4104061752ef
Bogdan Iorga <bogdan.iorga@icsn.cnrs-gif.fr>
2021-07-05 22:01:22 +0200

    time series for dihedrals of SM46 FEP water VDW 1000 extended to 1 microsecond uploaded to spudda

--- bin/RSYNC.sh
+++ bin/RSYNC.sh
@@ -3,3 +3,5 @@ rsync -avP spudda.beckstein.physics.asu.edu:/nfs/homes/bogdan/Projects/SAMPL7-Ph
 rsync -avP --exclude="*-dist.xvg.bz2" yamsolo.beckstein.physics.asu.edu:/nfs/homes3/sfan/Projects/SAMPL7/_dih_gaff .
 
 rsync -avP --exclude="*-dist.xvg.bz2" yamsolo.beckstein.physics.asu.edu:/nfs/homes3/sfan/Projects/SAMPL7/_dih_ligpargen .
+
+rsync -avP spudda.beckstein.physics.asu.edu:/nfs/homes/bogdan/Projects/SAMPL7-PhysProp/dih*ts_SM46_water_VDW_1000_extended_1microsec.xvg.gz .
