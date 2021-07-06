cb007958dab09e8c743eb3e77c86c1489a2eaf60
Oliver Beckstein <orbeckst@gmail.com>
2021-07-06 12:41:29 -0700

    analysis of extended simulations for SM46 OPLSAA VDW 1000 (gas)
    - updated RSYNC and SETUP scripts (rename files)
    - new notebook single_dihedrals.ipynb for custom plotting (lots of quick and dirty stuff
      in there...)

--- bin/RSYNC.sh
+++ bin/RSYNC.sh
@@ -4,4 +4,6 @@ rsync -avP --exclude="*-dist.xvg.bz2" yamsolo.beckstein.physics.asu.edu:/nfs/hom
 
 rsync -avP --exclude="*-dist.xvg.bz2" yamsolo.beckstein.physics.asu.edu:/nfs/homes3/sfan/Projects/SAMPL7/_dih_ligpargen .
 
+# rename these files in bin/SETUP.py
 rsync -avP spudda.beckstein.physics.asu.edu:/nfs/homes/bogdan/Projects/SAMPL7-PhysProp/dih*ts_SM46_water_VDW_1000_extended_1microsec.xvg.gz .
+rsync -avP spudda.beckstein.physics.asu.edu:/nfs/homes/bogdan/Projects/SAMPL7-PhysProp/dih[12]ts_SM46_octanol_VDW_1000_extended_466ns.xvg.gz .
--- bin/SETUP.sh
+++ bin/SETUP.sh
@@ -3,10 +3,25 @@
 TARFILE=_dih_analysis.tgz
 PARALLEL=$(dirname $0)/parallel.py
 
-tar xvf $TARFILE || { echo "Missing tar file $TARFILE"; exit 1; }
+# oplsaa and cgenff
+test -e $TARFILE && tar xvf $TARFILE || { echo "Missing tar file $TARFILE - SKIPPING"; }
+
 
 echo "removing histograms (not needed)"
 find . -name '*-dist.xvg' | xargs rm
 
 echo "compressing xvgs to save space (using 4 cores)"
 find . -name '*.xvg' | xargs ${PARALLEL} 4 bzip2 -v ---
+
+# purge xvg
+find . -name '*.xvg' | xargs rm
+
+# extended sims
+XDIR=_dih_oplsaa_extended
+echo "extended windows --> $XDIR"
+
+mkdir -p $XDIR
+for dih in 1 2; do
+    mv dih${dih}ts_SM46_water_VDW_1000_extended_1microsec.xvg.gz $XDIR/oplsaa-SM46-water-VDW-1000-dih${dih}-ts.xvg.gz
+    mv dih${dih}ts_SM46_octanol_VDW_1000_extended_466ns.xvg.gz $XDIR/oplsaa-SM46-octanol-VDW-1000-dih${dih}-ts.xvg.gz
+done
